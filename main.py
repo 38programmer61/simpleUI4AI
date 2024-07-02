e73ffd45fa52c16ff54a11f867109a9b40f915a0
Musa Nuri İhtiyar <56412256+38programmer61@users.noreply.github.com>
2024-07-02 14:25:42 +0300

    Write main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import streamlit as st
+
+def main():
+    load_dotenv()
+
+if __name__ == '__main__':
+    main()
