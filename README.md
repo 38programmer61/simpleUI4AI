191873f1e6eb81d1043e86afd5c2960c66b82f83
Musa <musaihtiyar@gmail.com>
2024-07-03 16:56:59 +0300

    More explanation about the system is added.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ To run the system
 2- Add a file called .env which contains API key values such as LANGCHAIN_API_KEY, OPENAI_API_KEY, LANGCHAIN_TRACING_V2 and STABILITY_API_KEY
 3- Use "pip install -r requirements.txt" to install the dependencies
 4- Run the system with "streamlit run Chat.py --server.fileWatcherType none"
+
+Python Version: 3.10
--- pages/Manual.py
+++ pages/Manual.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 st.title("How To Use")
-st.text("Explanation")
+st.text("It is possible to interact with the system in different ways.")
+st.text("1- Use the text prompt on the sidebar and click 'Send Text'")
+st.text("*** If the audio output is also desired, check audio output option as well.")
+st.text("2- You can send audio prompts as well. First activate Auditory Input. Then record your voice command. Lastly, click 'Send Recorded Audio' when you are ready.")
+st.text("3- If your prompt is in the following format 'Draw <description>, then instead of a verbal response, it will generate an image of the description")
+st.text("4- You can upload an image and click 'Send Image'. When this happens, it gives the verbal description of the provided image. If you activate voice output, in speech as well.")
+
+
