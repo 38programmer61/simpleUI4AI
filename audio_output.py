e42b5d4d6271beb0dcd5348e42d7cf278e161df8
Musa <musaihtiyar@gmail.com>
2024-07-03 16:24:19 +0300

    Unnecessary print statements were removed.

--- audio_output.py
+++ audio_output.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 from playsound import playsound
 
 
-def generate_audio_response(text: str, output_file_path: str = "speech.mp3") -> None:
+def generate_audio_response(text: str, output_file_path: str = ".speech.mp3") -> None:
     """
     Generate an audio response from the given text using OpenAI's text-to-speech service.
 
@@ -11,16 +11,14 @@ def generate_audio_response(text: str, output_file_path: str = "speech.mp3") -> 
         output_file_path (str): The path where the output audio file will be saved.
     """
     client = OpenAI()
-    print('Reached 1---')
     response = client.audio.speech.create(
         model="tts-1",
         voice="fable",
         input=text
     )
-    print('Reached 2---')
 
     response.stream_to_file(output_file_path)
-    print('Reached 3---')
+
 
 def play_sound(sound_path: str = "speech.mp3") -> None:
     """
@@ -29,6 +27,4 @@ def play_sound(sound_path: str = "speech.mp3") -> None:
     Args:
         sound_path (str): The path to the sound file to be played.
     """
-    print('Reached 4---')
     playsound(sound_path)
-    print('Reached 5---')
