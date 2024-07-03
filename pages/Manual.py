6cec05b91f05e1e0546350efcb77daa28a66a7ce
Musa <musaihtiyar@gmail.com>
2024-07-03 18:48:55 +0300

    Add more spacing for manual page.

--- pages/Manual.py
+++ pages/Manual.py
@@ -1,16 +1,22 @@
 import streamlit as st
 
 st.title("How To Use")
+st.text("")
 st.text("It is possible to interact with the system in different ways.")
+st.text("")
 st.text("1- Use the text prompt on the sidebar and click 'Send Text'")
 st.text("*** If the audio output is also desired, check audio output option as well.")
+st.text("")
 st.text("2- You can send audio prompts as well. First activate Auditory Input.")
 st.text("Then record your voice command.")
 st.text("Lastly, click 'Send Recorded Audio' when you are ready.")
+st.text("")
 st.text("3- If your prompt is in the following format 'Draw <description>', then ")
 st.text("instead of a verbal response, it will generate an image of the description.")
+st.text("")
 st.text("4- You can upload an image and click 'Send Image'.")
 st.text("When this happens, it gives the verbal description of the provided image.")
 st.text("If you activate voice output, in speech as well.")
+st.text("")
 st.text("It supports a number of languages!!!")
 
