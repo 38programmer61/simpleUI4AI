b5705f263e6a2d2585502c8bd7688d72e537a3c7
Musa <musaihtiyar@gmail.com>
2024-07-03 16:12:21 +0300

    More arbitary printing for debugging audio.

--- audio_output.py
+++ audio_output.py
@@ -11,14 +11,16 @@ def generate_audio_response(text: str, output_file_path: str = "speech.mp3") -> 
         output_file_path (str): The path where the output audio file will be saved.
     """
     client = OpenAI()
+    print('Reached 1---')
     response = client.audio.speech.create(
         model="tts-1",
         voice="fable",
         input=text
     )
+    print('Reached 2---')
 
     response.stream_to_file(output_file_path)
-
+    print('Reached 3---')
 
 def play_sound(sound_path: str = "speech.mp3") -> None:
     """
@@ -27,4 +29,6 @@ def play_sound(sound_path: str = "speech.mp3") -> None:
     Args:
         sound_path (str): The path to the sound file to be played.
     """
+    print('Reached 4---')
     playsound(sound_path)
+    print('Reached 5---')
